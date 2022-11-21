28bd0ba6416e40955371033fb13d05361c4a878d
Blake Smith <besmith43@gmail.com>
2022-11-20 19:09:34 -0600

    added homebrew

--- bashrc-set.sh
+++ bashrc-set.sh
@@ -8,10 +8,15 @@ if [ -d ~/Bin ]; then
     export PATH=~/Bin:$PATH
 fi
 
+if [ -d /home/linuxbrew/.linuxbrew/bin ]; then
+    export PATH=/home/linuxbrew/.linuxbrew/bin:$PATH
+fi
+
 export SYSTEMD_EDITOR=nvim
 export EDITOR=nvim
 
 alias ai="sudo apt install -y"
+alias as="apt search"
 alias au="sudo apt update && sudo apt upgrade"
 alias chtsh="curl https://cheat.sh/"
 
