6513cc3b237490110f11165157263b4962d4ba08
Blake Smith <besmith43@gmail.com>
2022-11-23 11:46:35 -0600

    adding podman install script

--- apt-install.sh
+++ apt-install.sh
@@ -13,8 +13,6 @@ sudo apt update && sudo apt install microsoft-edge-stable -y
 
 sudo apt install gimp audacity ansible python3-pip git golang-go dotnet-sdk-6.0 cmake powertop rclone clang pkg-config libssl-dev acpid acpi cpufrequtils linux-tools-common tmux fzf gnome-tweaks mpv pulseeffects ardour gnupg2 caffeine -y
 
-sudo apt install podman podman-docker podman-compose podman-toolbox -y
-
 sudo apt install libfuse2 -y 
 
 sudo snap install powershell --classic
---
+++ podman-install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt install podman podman-docker podman-toolbox -y
+
+flatpak install flathub io.podman_desktop.PodmanDesktop
+
+pip3 install podman-compose
+
